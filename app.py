96d5c21a136c97449032d5babdccce258b1eb35b
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-12-17 14:43:39 -0600

    feat: add data layout

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ data_screen = uic.loadUi("data.ui")
 # Executable
 login_screen.show()
 
-# def init():
 login_screen.error_label.hide()
+data_screen.error_label.hide()
 attempts = 2
 
 
@@ -30,16 +30,43 @@ def gui_login():
         login_screen.close()
 
     else:
-        login_screen.error_label.setText(f"Clave incorrecta. Quedan {attempts} intentos")
+        login_screen.error_label.setText(
+            f"Clave incorrecta. Quedan {attempts} intentos")
         attempts -= 1
         login_screen.password.clear()
 
 
-def gui_close():
-    close_screen.close()
+def app_exit():
+    app.exit()
+
+
+def gui_data():
+    audio_compression = data_screen.audio_compression.text().split("%")[0]
+
+    video_compression = data_screen.video_compression.text().split("%")[0]
+    video_initial_time = data_screen.video_initial_time.text().split(":")
+    video_final_time = data_screen.video_final_time.text().split(":")
+
+    if audio_compression == '' or video_compression == '' or video_initial_time[0] == '' or video_final_time[0] == '':
+        data_screen.error_label.show()
+        return
+
+    data_screen.error_label.hide()
+    initial_time = int(video_initial_time[0]) * 60 + int(video_initial_time[1])
+    final_time = int(video_final_time[0]) * 60 + int(video_final_time[1])
+    duration = final_time - initial_time
+
+    audio_bits = data_screen.audio_bits.currentText()
+    audio_frequency = data_screen.audio_frequency.currentText()
+    audio_channels = data_screen.audio_channels.currentText()
+
+    video_color = data_screen.video_color.currentText()
+    video_resolution = data_screen.video_resolution.currentText()
+    video_fps = data_screen.video_fps.currentText()
 
 
 login_screen.login.clicked.connect(gui_login)
-close_screen.btn_close.clicked.connect(gui_close)
+close_screen.btn_close.clicked.connect(app_exit)
+data_screen.calculate.clicked.connect(gui_data)
 
 app.exec()
